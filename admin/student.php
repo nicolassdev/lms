b21495a0c648a9d4be1b22dba9bee7ecabe0562e
Anthony <anthonydaen25@gmail.com>
2025-01-21 01:08:54 +0800

    done

--- admin/student.php
+++ admin/student.php
@@ -26,9 +26,11 @@ include "../admin/includes/Forms/studentform.php";
 </style>
 
 
+<!-- MODAL -->
+
+
+
 <!-- TABLE -->
-
-
 <main class="col-md-12 ms-sm-auto col-lg-10 px-md-4">
     <div class="container">
         <div class="row">
@@ -476,5 +478,3 @@ include "../admin/includes/Forms/studentform.php";
 <script>
     initializeDataTable("studentRecord", 7, "Student Records");
 </script>
- 
- 
