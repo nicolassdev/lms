48a26767f361dcf97a3af91616ab5111f270030f
Anthony Daen <113879059+nicolassdev@users.noreply.github.com>
2025-01-20 20:57:43 +0800

    Update dbh-inc.php

--- includes/dbh-inc.php
+++ includes/dbh-inc.php
@@ -1,3 +1,3 @@
 <?php
 include "function.php";
-$mySQLFunction = new myDataBase("	jsftj8ez0cevjz8v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "dui2ff3pt215060n", "y45buvc5ksp80l01", "rw73o3ap7n2qe5xr");
+$mySQLFunction = new myDataBase("jsftj8ez0cevjz8v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "dui2ff3pt215060n", "y45buvc5ksp80l01", "rw73o3ap7n2qe5xr");
