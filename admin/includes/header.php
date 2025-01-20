32137fef0e426e3628013f5f9d391c8469782f40
Anthony <anthonydaen25@gmail.com>
2025-01-20 23:14:20 +0800

    update

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -13,12 +13,11 @@
     <!-- <link href="./css/bootstrap.min.css" rel="stylesheet"> -->
     <!-- <link rel="stylesheet" href="../../assets/bootstrap-5.3.3-dist/css/bootstrap.min.css"> -->
     <link rel="stylesheet" href="../assets/css/admin.css?v=<?php echo time(); ?>" />
-
-    <!-- this is material icon  -->
-    <!-- <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet"> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css"> <!-- SET THE ICON  -->
+    <!-- SET THE ICON  -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet" />
 
@@ -30,11 +29,9 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.6/css/buttons.dataTables.min.css">
 
 
-    <!-- Include Morris.js and jQuery -->
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+
     <!-- Include Animate.css -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
 
