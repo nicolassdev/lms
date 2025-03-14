82a4cf96d6361caedefd1999f9dccf55d595c994
Anthony Daen <113879059+nicolassdev@users.noreply.github.com>
2025-03-14 23:47:23 +0800

    Update dbh-inc.php

--- includes/dbh-inc.php
+++ includes/dbh-inc.php
@@ -1,3 +1,8 @@
 <?php
 include "function.php";
-$mySQLFunction = new myDataBase("jsftj8ez0cevjz8v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "dui2ff3pt215060n", "y45buvc5ksp80l01", "rw73o3ap7n2qe5xr");
+$mySQLFunction = new myDataBase(
+  "jsftj8ez0cevjz8v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",    // host
+  "dui2ff3pt215060n",                                             // Username         
+  "y45buvc5ksp80l01",                                             // Password     
+  "rw73o3ap7n2qe5xr"                                              // Database  
+);
