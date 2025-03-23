e6a41eea7675dca62a9ebd0c798c325cb565b1b7
Anthony <anthonydaen25@gmail.com>
2025-03-24 01:12:44 +0800

    DONE ~~~~

--- includes/student-header.php
+++ includes/student-header.php
@@ -103,7 +103,7 @@ error_reporting(E_ALL);
     <header>
         <div class="container-fluid">
             <div class="row mt-5">
-                <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block  sidebar offcanvas-md offcanvas-start" style="max-width: 250px;">
+                <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block  sidebar offcanvas-md offcanvas-start lms-scroll-bar" style="max-width: 250px;">
                     <div class="position-sticky">
                         <!-- <h3 class="mb-4">Dashboard</h3> -->
                         <small class="text-white ms-2 d-lg-none mt-2">
